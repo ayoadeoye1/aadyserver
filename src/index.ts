74efe07321fa5ae9286ebd8bd1fbe430d7be49af
ayoadeoye1 <isadeoye02@gmail.com>
2023-06-14 16:01:34 +0100

    listening issue

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,42 @@
-import * as http from 'http';
-import app from './app';
+import * as express from 'express';
+import * as dotenv from 'dotenv';
+import * as cors from 'cors';
+import * as mongoose from 'mongoose';
 
-const server = http.createServer(app);
+import router from './routes';
+dotenv.config();
 
 const PORT = process.env.PORT;
 
-server.listen(PORT, () =>
-    console.log(`listening on port: ${PORT}`)
-)
+const app = express();
+
+const MONGO_URL = process.env.MONGO_URL;
+
+const dbConnect = async() =>{
+    try {
+        await mongoose.connect(MONGO_URL, {
+          dbName: 'portifolio'
+        });
+        console.log('DB connected')
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+dbConnect().then(() =>{
+  app.listen(PORT, () => {
+    console.log(`listening for requests on port ${PORT}`);
+  })
+})
+
+app.use(cors({
+    origin: "*"
+}));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', router);
+
+
+export default app;
