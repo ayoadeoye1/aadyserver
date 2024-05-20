75b8dbd058f824dcd3f3f5fc4fad6db068a10df2
ayoadeoye1 <isadeoye02@gmail.com>
2024-05-20 05:04:29 +0100

    small fix in porr code

--- src/routes.ts
+++ src/routes.ts
@@ -1,73 +1,68 @@
-import * as express from 'express';
+import * as express from "express";
 
-import upload from './utils/multer';
-import { Aboutmeget, Aboutmepatch } from './controllers/about'
-import { Skillsdelete, Skillsget, Skillspost } from './controllers/skills';
-import { Projdelete, Projget, Projpost } from './controllers/projects';
-import { Blogclap, Blogdelete, Blogget, Bloggetone, Blogpost } from './controllers/blog';
-import { Edudelete, Eduget, Edupost } from './controllers/education';
-import { Refdelete, Refget, Refjpost } from './controllers/references';
-import { Mesjdelete, Mesjget, Mesjpost } from './controllers/message';
-import { AdminLogin, AdminReg } from './auth/auth.admin';
-import { Userdataget, Usersdata } from './controllers/userinfo';
-import { Authorize } from './middlewares/authorization';
-import * as cors from 'cors';
+import upload from "./utils/multer";
+import { Aboutmeget, Aboutmepatch } from "./controllers/about";
+import { Skillsdelete, Skillsget, Skillspost } from "./controllers/skills";
+import { Projdelete, Projget, Projpost } from "./controllers/projects";
+import {
+    Blogclap,
+    Blogdelete,
+    Blogget,
+    Bloggetone,
+    Blogpost,
+} from "./controllers/blog";
+import { Edudelete, Eduget, Edupost } from "./controllers/education";
+import { Refdelete, Refget, Refjpost } from "./controllers/references";
+import { Mesjdelete, Mesjget, Mesjpost } from "./controllers/message";
+import { AdminLogin, AdminReg } from "./auth/auth.admin";
+import { Userdataget, Usersdata } from "./controllers/userinfo";
+import { Authorize } from "./middlewares/authorization";
+import * as cors from "cors";
 
 const router = express.Router();
 
-
-
 //-----auth routes------
-router.post('/sign-in', cors(), AdminLogin);
-router.post('/sign-up', Authorize, AdminReg); //secured
-
+router.post("/sign-in", AdminLogin);
+router.post("/sign-up", AdminReg); //secured
 
 //-----about routes------
-router.get('/aboutmeget/:id', Aboutmeget);
-router.patch('/aboutmepatch/:id', Authorize, Aboutmepatch);
-
+router.get("/aboutmeget/:id", Aboutmeget);
+router.patch("/aboutmepatch/:id", Authorize, Aboutmepatch);
 
 //-----skills routes------
-router.get('/skillsget', Skillsget)
-router.post('/skillspost', Authorize, Skillspost)
-router.delete('/skillsdel/:id', Authorize, Skillsdelete)
-
+router.get("/skillsget", Skillsget);
+router.post("/skillspost", Authorize, Skillspost);
+router.delete("/skillsdel/:id", Authorize, Skillsdelete);
 
 //-----projects routes------
-router.get('/projectsget', Projget);
-router.post('/projectspost', Authorize, Projpost);
-router.delete('/projectsdel/:id', Authorize, Projdelete);
-
+router.get("/projectsget", Projget);
+router.post("/projectspost", Authorize, Projpost);
+router.delete("/projectsdel/:id", Authorize, Projdelete);
 
 //-----blog routes------
-router.get('/blogget', Blogget);
-router.get('/bloggetone/:id', Bloggetone);
-router.post('/blogpost', Authorize, upload.single('image'), Blogpost);
-router.delete('/blogdel/:id', Authorize, Blogdelete);
-router.put('/blogclap/:id', Blogclap);
-
+router.get("/blogget", Blogget);
+router.get("/bloggetone/:id", Bloggetone);
+router.post("/blogpost", Authorize, upload.single("image"), Blogpost);
+router.delete("/blogdel/:id", Authorize, Blogdelete);
+router.put("/blogclap/:id", Blogclap);
 
 //-----education routes------
-router.get('/eduget', Eduget);
-router.post('/edupost', Authorize, Edupost);
-router.delete('/edudel/:id', Authorize, Edudelete);
-
+router.get("/eduget", Eduget);
+router.post("/edupost", Authorize, Edupost);
+router.delete("/edudel/:id", Authorize, Edudelete);
 
 //-----references routes------
-router.get('/refget', Refget);
-router.post('/refpost', Authorize, Refjpost);
-router.delete('/refdel/:id', Authorize, Refdelete);
-
+router.get("/refget", Refget);
+router.post("/refpost", Authorize, Refjpost);
+router.delete("/refdel/:id", Authorize, Refdelete);
 
 //-----message routes------
-router.get('/msgget', Authorize, Mesjget); //secured
-router.post('/msgpost', Mesjpost); //non secured
-router.delete('/msgdel/:id', Authorize, Mesjdelete);
-
+router.get("/msgget", Authorize, Mesjget); //secured
+router.post("/msgpost", Mesjpost); //non secured
+router.delete("/msgdel/:id", Authorize, Mesjdelete);
 
 //-----user data route------
-router.post('/ipdata', Usersdata);
-router.get('/ipdataget', Authorize, Userdataget);
+router.post("/ipdata", Usersdata);
+router.get("/ipdataget", Authorize, Userdataget);
 
-
-export default router
+export default router;
--- src/security/bcrypt.ts
+++ src/security/bcrypt.ts
@@ -1,13 +1,11 @@
-import * as bcrypt from 'bcryptjs'
+import * as bcrypt from "bcrypt";
 
-export class Bcrypt{
+export class Bcrypt {
+    public static async encryptPsw(password: string) {
+        return await bcrypt.hash(password, 12);
+    }
 
-  public static async encryptPsw(password: string){
-    return await bcrypt.hash(password, 20);
-  }
-
-  public static async validatePsw(password: string, hpassword: string){
-    return await bcrypt.compare(password, hpassword);
-  }
-
+    public static async validatePsw(password: string, hpassword: string) {
+        return await bcrypt.compare(password, hpassword);
+    }
 }
