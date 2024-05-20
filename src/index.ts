84127460e6095e96475b87c326554b32a6ac4c28
ayoadeoye1 <isadeoye02@gmail.com>
2024-05-20 04:21:38 +0100

    cors listing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import * as express from 'express';
-import * as dotenv from 'dotenv';
-import * as cors from 'cors';
-import * as mongoose from 'mongoose';
+import * as express from "express";
+import * as dotenv from "dotenv";
+import * as cors from "cors";
+import * as mongoose from "mongoose";
 
-import router from './routes';
+import router from "./routes";
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -12,31 +12,36 @@ const app = express();
 
 const MONGO_URL = process.env.MONGO_URL;
 
-const dbConnect = async() =>{
+const dbConnect = async () => {
     try {
         await mongoose.connect(MONGO_URL, {
-          dbName: 'portifolio'
+            dbName: "portifolio",
         });
-        console.log('DB connected')
+        console.log("DB connected");
     } catch (error) {
         console.log(error.message);
     }
-}
+};
 
-dbConnect().then(() =>{
-  app.listen(PORT, () => {
-    console.log(`listening for requests on port ${PORT}`);
-  })
-})
+dbConnect().then(() => {
+    app.listen(PORT, () => {
+        console.log(`listening for requests on port ${PORT}`);
+    });
+});
 
-app.use(express.json({ limit: '10mb' }))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: ['https://ayoadeoye-portfolio.netlify.app'],
-  methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true 
-}));
+app.use(
+    cors({
+        origin: [
+            "https://ayoadeoye-portfolio.netlify.app",
+            "http://localhost:3000",
+        ],
+        methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
 
-app.use('/api', router);
+app.use("/api", router);
